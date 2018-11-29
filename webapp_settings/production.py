d57ae03634d6158af8bea9b269530ee7dcf2ff90
Sym Roe <sym.roe@talusdesign.co.uk>
2018-11-29 10:08:37 +0000

    Latest deployed config

--- webapp_settings/production.py
+++ webapp_settings/production.py
@@ -53,7 +53,8 @@ GOCARDLESS_ACCESS_TOKEN = "{{ vault_gocardless_access_token }}"
 
 CHECK_HOST_DIRTY = True
 DIRTY_FILE_PATH = "~/server_dirty"
-EE_BASE = "http://localhost:8000"
+EE_BASE = "https://elections.democracyclub.org.uk"
+
 
 EMAIL_SIGNUP_ENDPOINT = 'https://democracyclub.org.uk/mailing_list/api_signup/v1/'
 EMAIL_SIGNUP_API_KEY = '{{ vault_email_signup_api_key }}'
