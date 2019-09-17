38ca2cdea95718a9582d8e966d5926a6c9877b9c
Sym Roe <sym.roe@talusdesign.co.uk>
2019-09-17 10:05:51 +0100

    Update Image

--- webapp_settings/production.py
+++ webapp_settings/production.py
@@ -53,7 +53,7 @@ GOCARDLESS_ACCESS_TOKEN = "{{ vault_gocardless_access_token }}"
 
 CHECK_HOST_DIRTY = True
 DIRTY_FILE_PATH = "~/server_dirty"
-#EE_BASE = "http://localhost:8000"
+# EE_BASE = "http://localhost:8000"
 
 
 EMAIL_SIGNUP_ENDPOINT = 'https://democracyclub.org.uk/mailing_list/api_signup/v1/'
@@ -66,3 +66,6 @@ sentry_sdk.init(
     dsn="{{ vault_sentry_dsn }}",
     integrations=[DjangoIntegration()]
 )
+
+# STRIPE_API_KEY = "sk_test_mlQaYWX7AcoD5s05d0AoTsDn"
+STRIPE_API_KEY = "sk_live_j8gBH6npHjtWWgvUpH5dKYim"
