f4370f2cb2b9591dc686c7be283b052b55da6318
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-08-25 10:01:08 +0100

    Update remove_db_replication script
    - Use while loop to repeat drop replication command until success
    - Resolves issue where an active replication would fail to drop

--- files/scripts/remove_db_replication.sh
+++ files/scripts/remove_db_replication.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-set -x
+set -ex
 
 USER={{ project_name }}
 INSTANCE_ID=`curl http://instance-data/latest/meta-data/instance-id`
 SUBSCRIPTION=${USER}_${INSTANCE_ID:2}
 
-psql -U $USER -c "DROP SUBSCRIPTION $SUBSCRIPTION;"
+drop_subscription () {
+    psql -U $USER -c "DROP SUBSCRIPTION $SUBSCRIPTION;"
+}
+
+# if subscription is active it will fail - repeat until inactive
+until drop_subscription; do
+    echo "Trying to drop subscription again..."
+done
+
+echo "Subscription dropped"
 touch {{ project_root }}/home/server_dirty
