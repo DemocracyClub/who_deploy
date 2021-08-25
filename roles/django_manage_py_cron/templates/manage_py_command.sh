826d99d7e4de547f897f08882dd1fd93e91d7a72
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-08-25 16:44:46 +0100

    Remove tasks no longer required in provisioning
    - No longer required as these will be managed by db replication and
    reload systemd
    - Remove step to include webapp settings
    - Remove database migrate and initial management commands
    - Remove controller instance tagging
    - Remove wcivf database init data tasks
    - Remove cron roles

--- roles/django_manage_py_cron/templates/manage_py_command.sh
+++
@@ -1,8 +0,0 @@
-# This is a script that makes runnning manage.py inside a virtualenv from
-# cron easier.
-# It does a few thigns:
-#     1. It wraps comands in `output-on-error`
-#     2. It calls the manage.py with the virtualenv's python
-# It required a var `{{ project_name }}` and for the project to be at
-# /var/www/{{ project_name }}
-/usr/bin/output-on-error /var/www/{{ project_name }}/env/bin/python /var/www/{{ project_name }}/code/manage.py "$@"
