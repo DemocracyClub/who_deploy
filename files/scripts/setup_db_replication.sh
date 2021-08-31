a3af19139b6ee6e3723be26ffa7130c1f5e37e31
Michael Collins <michaeljcollinsuk@users.noreply.github.com>
2021-08-31 16:41:59 +0100

    Use env vars in setup DB replication script

--- files/scripts/setup_db_replication.sh
+++ files/scripts/setup_db_replication.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -xeE
 
-DB={{ project_name }}
+# load .env file to get RDS secrets
+export $(cat ./code/.env | xargs)
 USER={{ project_name }}
-RDS_HOST={{ vault_rds_host }}
-RDS_PASSWORD='{{ vault_rds_password }}'
+DB={{ project_name }}
 INSTANCE_ID=`curl http://instance-data/latest/meta-data/instance-id`
 SUBSCRIPTION=${USER}_${INSTANCE_ID:2}
 
 dropdb --if-exists $DB -U $USER
 createdb $DB -U $USER
 source {{ project_root }}/env/bin/activate && {{ project_root }}/code/manage.py migrate
-psql $DB -U $USER -c "CREATE SUBSCRIPTION $SUBSCRIPTION CONNECTION 'dbname=$DB host=$RDS_HOST user=$USER password=$RDS_PASSWORD' PUBLICATION alltables;"
+psql $DB -U $USER -c "CREATE SUBSCRIPTION $SUBSCRIPTION CONNECTION 'dbname=$RDS_DB_NAME host=$RDS_HOST user=$USER password=$RDS_DB_PASSWORD' PUBLICATION alltables;"
 rm -f /var/www/wcivf/home/server_dirty
